e25cdb2a05a0bcf5296482a3f70053df583cbca1
bismaiftikhar21 <bismaiftikhar21@gmail.com>
2025-12-17 17:35:30 +0500

    Update resultsView.js

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,19 +1,4 @@
 
-/*
-
-import View from "./View.js";
-
-class ResultsView extends View {
-    _parentEl = document.querySelector('.results');
-}
-
-export default new ResultsView();
-
-
-
-
-
-*/
 
 
 import View from './View.js';
